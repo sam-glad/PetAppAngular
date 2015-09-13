7b505a022d31b710a3c661a47dff8326cbd91c52
Sam Gladstone <gladston@umich.edu>
2015-09-13 17:07:35 -0400

    Finish basic organization index page

--- app/scripts/controllers/organizations.js
+++ app/scripts/controllers/organizations.js
@@ -3,4 +3,12 @@
 angular.module('petApp')
   .controller('OrganizationsCtrl', function ($scope, organizationsService) {
       $scope.organizations = organizationsService.getAllOrganizations();
+
+      $scope.getContactInfo = function(organization) {
+        if (organization.phone_preferred) {
+          return organization.phone_number.toString() + organization.phone_extension;
+        }
+
+        return organization.email_address;
+      };
   });
