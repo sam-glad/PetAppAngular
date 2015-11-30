64fb84bbf409931351938e04bfb9c6c571062eb3
Sam Gladstone <gladston@umich.edu>
2015-11-30 16:20:41 -0500

    Touch up directive and apply where necessary

--- app/scripts/controllers/petapplications/show.js
+++ app/scripts/controllers/petapplications/show.js
@@ -3,4 +3,13 @@
 angular.module('petApp')
   .controller('PetApplicationShowCtrl', function ($scope, petApplicationsPrepService) {
     $scope.petApplication = petApplicationsPrepService;
+    transformForDirective($scope.petApplication);
+
+    function transformForDirective(petApplication) {
+      petApplication.questions.forEach(function (question) {
+        if (!question.answersGiven) {
+          question.answersGiven = question.answers;
+        }
+      });
+    }
   });
