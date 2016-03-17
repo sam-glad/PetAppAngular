dfd3d9b0284085508cb66c280c63112b6e8c0c91
Sam Gladstone <gladston@umich.edu>
2016-03-17 12:21:52 -0400

    Only select pending applications for home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,10 @@
 
 angular.module('petApp')
   .controller('MainCtrl', function ($scope, $rootScope, User, UtilsService, PET_APPLICATION_STATUSES, petApplicationService) {
+    $scope.Utils = UtilsService;
     if ($scope.user.id && $scope.user.adminOrganizations.length > 0) {
-      petApplicationService.getAdminPetApplications($rootScope.user.id)
+      petApplicationService.getPendingAdminPetApplications($scope.user.id)
         .then(function (petApplications) {
-          $scope.Utils = UtilsService;
           $scope.petApplications = petApplications;
         });
     }
--- app/scripts/factories/petapplicationservice.js
+++ app/scripts/factories/petapplicationservice.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('petApp')
-  .factory('petApplicationService', function(Restangular, PetApplication, UtilsService) {
+  .factory('petApplicationService', function(Restangular, PetApplication, UtilsService, PET_APPLICATION_STATUSES) {
     var service = {
       getPetApplication: getPetApplication,
       postPetApplication: postPetApplication,
       getPetApplicationsByOrganizationId: getPetApplicationsByOrganizationId,
       getAdminPetApplications: getAdminPetApplications,
+      getPendingAdminPetApplications: getPendingAdminPetApplications,
       putPetApplication: putPetApplication
     };
 
@@ -24,6 +25,14 @@ angular.module('petApp')
       return Restangular
         .service('admin_pet_applications', Restangular.one('users', userId))
           .getList().then(function(responseData) {
+            return UtilsService.buildModelsFromResponse(responseData, PetApplication);
+          });
+    }
+
+    function getPendingAdminPetApplications(userId) {
+      return Restangular
+        .service('admin_pet_applications', Restangular.one('users', userId))
+          .getList({status: PET_APPLICATION_STATUSES.pending.id}).then(function(responseData) {
             return UtilsService.buildModelsFromResponse(responseData, PetApplication);
           });
     }
